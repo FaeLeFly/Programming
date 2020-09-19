0b2e80ce70499a8087d2982a8d5fc89ddf462827
FaeLeFly <contriigor@gmail.com>
2020-09-19 12:46:23 +0300

    Задания 1-5

---
+++ Projects/01/C++/1 задание/1 задание/Source.cpp
@@ -0,0 +1,8 @@
+#include <iostream>;
+int main() {
+	using namespace std;
+	setlocale(LC_ALL, "Russian");
+	cout << "результат выражения 2+2*2=";
+	cout << 2 + 2 * 2;
+	return 0;
+}
---
+++ Projects/03/Phyton/задание 3.py
@@ -0,0 +1,7 @@
+a = int( input ())
+b = int (input ())
+s= a + b
+d= a-b
+f=a*b
+g=a/b
+print (s,"\n",d,"\n",f,"\n",g,"\n")
