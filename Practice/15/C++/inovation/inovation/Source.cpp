24a970fb4b672b0d4e2a9363d94271f7a23fff0a
FaeLeFly <contriigor@gmail.com>
2020-09-29 12:34:22 +0300

    1

---
+++ Practice/15/C++/inovation/inovation/Source.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>;
+#include <cstdlib>;
+#include <ctime>;
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian") ;
+	int magic, guess;
+	char da('1');
+	srand(time(0));
+	da:
+	magic = rand() % 100 + 1;
+	cout << "{ГПСЧ}\t" << '\n';
+	for (int count = 0; count < 5; count++) {
+		cin >> guess;
+		if (magic == guess) {
+			cout <<  "\nПоздравляю! Вы угадали. "<< "Было загадано\t"<<magic<<'\n';
+			cout << "Хотите начать сначала? " <<"  "<< "{1-ДА}";
+			cin >> da;
+			if (da == '1')goto da; else return 0;
+		}
+		if (magic < guess)cout << "Загаданное число меньше\n"; else cout << "Загаданное число больше\n";
+		if ((count + 1) % 5 == 0) {
+			count = 0;
+			cout << "\nВы проиграли. Было загадано:\t" << magic<<'\n';
+			cout << "Хотите начать сначала?" << "{1-ДА}";
+			cin >> da;
+			if (da=='1') goto da; else  return 0;
+		}
+	}
+	system("pause");
+	return 0;
+}
