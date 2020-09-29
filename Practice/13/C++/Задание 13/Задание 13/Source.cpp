46b6b1136989bd96dd55e6ba49bd41694d50f46b
FaeLeFly <contriigor@gmail.com>
2020-09-29 12:32:15 +0300

    1

---
+++ Practice/13/C++/Задание 13/Задание 13/Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>;
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	int n; cin >> n;
+	if ((n / 1) && (n / n) && !((n % 2)==0) && !((n % 3)==0)|| n==2||n==3) cout << "Простое\n";
+	else if ((n / 2) || (n / 3))cout << "Составное\n";
+	system("pause");
+		return 0;
+}
