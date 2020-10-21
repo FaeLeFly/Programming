bb0945f71779e6835f55d103f09c5d09f720beee
FaeLeFly <contriigor@gmail.com>
2020-10-22 00:00:10 +0300

    1

--- Practice/01/C++/1 задание/1 задание/Source.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>;
-int main() {
-	using namespace std;
-	cout << "Результат выражения 2+2*2=", 2 + 2 * 2;
-	return 0;
-}
--- Practice/13/Python/задание13.py
+++
@@ -1,7 +0,0 @@
-import os
-n=int(input())
-if (n/1 and n/n and not((n%2)==0) and not((n%3)==0) or n==2 or n==3):
-    print ("Простое",'\n')
-elif (n/2 or n/3):
-    print ("Составное",'\n')
-os.system ("pause")
