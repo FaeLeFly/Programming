e4b0640418f5b3cbaedb98976fca98809131163e
FaeLeFly <contriigor@gmail.com>
2021-03-03 22:25:04 +0300

    1

---
+++ Practice/34/C++/ke34/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+void create(int **ci,int m,int first,int range){
+    *ci=new int[m];
+    int count=first;
+     for (int i=0;i<m;i++){
+         if (i!=0){count+=range;}
+         (*ci)[i]=count;
+        }
+}
+void destroy(int **ci){
+    delete [] (*ci);
+    *ci=nullptr;
+}
+int sort(int *ci,int m){
+    int ne;
+        for(int i=0;i<m;i++){
+            for (int j=0;j<m-1;j++){
+                if (ci[j]>ci[j+1]){
+                    ne=ci[j+1];
+                    ci[j+1]=ci[i];
+                    ci[i]=ne;
+                }
+                else{
+                    continue;
+                }
+            }
+        }
+        return *ci;
+}
+int print(int *ci,int m){
+    for (int i=0;i<m;i++){
+            std::cout<<ci[i];
+        }
+    return *ci;
+}
+int main()
+{
+    int m,first=0,range=0;
+    std::cin>>m; std::cin>>first; std::cin>>range;
+    int *ci;
+    create(&ci,m,first,range);
+    *ci=sort(ci,m);
+    print(ci,m);
+    destroy(&ci);
+    return 0;
+}
